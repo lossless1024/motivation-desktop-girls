b1f1269608cfa6031809ba4a44bf2fa50348e466
lossless1024 <lossless1024@gmail.com>
2021-04-11 03:34:02 +0200

    Fixes

--- dl.py
+++ dl.py
@@ -1,17 +1,29 @@
 import requests
 from multiprocessing import Pool
-base = 'https://erowall.com/tf558550ef6e/'
+from itertools import product
 
-def dl(n):
+
+hosts = [
+#	'https://erowall.com/tf558550ef6e/',
+	'https://zoomgirls.net/t00a0b00821f/',
+	'http://girlwallpaper.pro/t08ece370cf3/',
+	'https://www.adultwalls.com/td480c6bc2c7/',
+	'https://nakedgirlwallpaper.pro/totempoping/t354881a7582/',
+]
+
+def dl(*args):
+	host, n = args
 	for ch in [chr(c) for c in range(ord('a'), ord('f')+1)]:
-		for i in range(0, 10000):
+		for i in range(0, 2000):
 			url = '{:s}{:04d}_{:1d}.mp4'.format(ch, i, n)
-			resp = requests.get(base + url)
+			resp = requests.get(host + url)
 			if resp.status_code == 200:
-				print(base + url)
+				print(host + url)
 				with open('videos/' + url, 'wb') as f:
 					f.write(resp.content)
 
 if __name__ == '__main__':
-	with Pool(10) as p:
-		p.map(dl, range(10))
+	for host in hosts:
+		with Pool(10) as p:
+			p.starmap(dl, product([host], range(10)))
+
--- girly.sh
+++ girly.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd $(dirname $(realpath $0))
 file=$(ls -1 videos | grep mp4 | shuf -n 1)
-python3 strip.py "$(realpath videos/$file)"
+python3 strip.py "videos/$file"
